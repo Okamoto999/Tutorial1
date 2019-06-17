2ef2252323b225a30c8f5942960ae7a493518f11
okamoto999 <okamoto.yuuki.infratop@gmail.com>
2019-06-17 16:47:43 +0900

    [FInish]3-4

---
+++ RubyRails/app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+ 
+  def new
+       @post = Post.new
+  end
+  
+  # 以下を追加
+  def create
+      # ストロングパラメーターを使用
+       post = Post.new(post_params)
+      # DBへ保存する
+       post.save
+      # 新規投稿画面へリダイレクト
+      redirect_to '/posts/new'
+  end
+
+  private
+ 
+  def post_params
+      params.require(:post).permit(:title, :body, :category)
+  end
+end
