dbf218236d864aff16d57c08f90acd9945ab581a
okamoto999 <devgobouman@gmail.com>
2019-06-23 16:57:55 +0900

    [Finish]4-7

--- CARAVAN2/app/controllers/blogs_controller.rb
+++ CARAVAN2/app/controllers/blogs_controller.rb
@@ -23,6 +23,11 @@ class BlogsController < ApplicationController
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
+  def destroy
+    blog=Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
 
   def index
     @blogs=Blog.all
